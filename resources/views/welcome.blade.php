5979031192d34b29d5c40c07b95978ae59fe3308
SemTK <semdekoning04@gmail.com>
2022-09-20 10:33:50 +0200

    added font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="app.css">
     <title>Curio Open Dag | Sem de Koning</title>
 </head>
@@ -15,7 +18,7 @@
                     <a href="">Home</a>
                     <a href="">Rooster Info</a>
                     <a href="">Smoelenboekje</a>
-                    <a href="   ">Algemene info</a>
+                    <a href="">Algemene info</a> 
                 </nav>
             </div>
         </div>
