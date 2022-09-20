94aa81bfc6d1deddab6a4599a41ee99328d5b8df
SemTK <semdekoning04@gmail.com>
2022-09-20 09:39:51 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title>Curio Open Dag | Sem de Koning</title>
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="wrapper">
+                <img src="curio-02-wit-logo-rgb.png" alt="Mooi plaatje">
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Rooster Info</a>
+                    <a href="">Smoelenboekje</a>
+                    <a href="   ">Algemene info</a>
+                </nav>
+            </div>
+        </div>
+        
+        
+    </header>
+</body>
+</html>
