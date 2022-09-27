4d57632afbf7d5f9a4d35621c3786b47240c09e4
SemTK <semdekoning04@gmail.com>
2022-09-27 11:07:50 +0200

    created rooster img and rooster info in 2 classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,14 +17,31 @@
                 <nav>
                     <a href="">Home</a>
                     <a href="">Algemene info</a> 
-                    <a href="">Rooster Info</a>
+                    <a href="#roosterInfo">Rooster Info</a>
                     <a href="">Smoelenboekje</a>              
                 </nav>
             </div>
         </div>       
     </header>
     <main>
-        <img src="rooster.jpg" alt="Rooster van SSD1A">
+        <div class="rooster">
+            <img src="rooster.jpg" alt="Rooster van SSD1A">
+            <div class="rooster-info" id="roosterinfo">
+                <p>Dit is een rooster zoals je deze in het eerste jaar van je opleiding kan verwachten.<br>
+                Het is een rooster met niet al te veel uren, die zich zullen opbouwen hoe verder je in de opleiding komt.<br> Hier is wat informatie over de vakken die je zult krijgen als je hier gaat studeren.
+                <br><br>
+                Ten eerste heb je de 3 standaardvakken: Nederlands, Engels, en dan rekenen. Deze spreken voorzich, de niveau's worden op 3F afgenomen. Dan heb je Studieloopbaanbegeleiding. Dit is je mentor uur.<br>
+                Dan is er het vak "Windows Development". In dit vak ga je echt leren programmeren, zoals de naam al zegt. Voor windows applicaties. Dus je zult beginnen met programma's maken voor op je "Console" en zult later in het jaar uitbreiden naar echte visuele apps.<br>
+                Dan volgt het vak "Web Development". In dit vak ga je met websites werken. Dit houdt dus in dat je inplaats van op het internet gaat surfen. Je zelf websites gaat maken, je gaat websites maken met allerlei onderwerpen en inhoud.<br><br>
+                Daarna volgt het vak "Praktijkwerk". In dit vak ga je werken zoals je in het echt ook zou werken in dit gebied. Je werkt in groepen aan projecten met deadlines, over de kennis die je tegelijkertijd leert bij Windows Development en Web Development.  
+                In dit vak heerst ook de kantoorsfeer.<br><br>
+                En als laatste heb je "Praktijkondersteuning". In dit vak leer je alles over programmeren zonder te programmeren. Je leert te presenteren, interviews te houden, de klant informatie te geven en informatie van de klant te ontvangen. Je leert te plannen en je leert hoe de werksfeer en werksituatie er nou eigenlijk echt uitziet en hoe dat allemaal gaat.<br>
+                </p>            
+            </div>
+        </div>
+        <div class="algemene-info">
+            <p>testestest</p>
+        </div>
     </main>
 </body>
 </html>
